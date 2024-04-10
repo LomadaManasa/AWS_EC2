ac76f3edca407a0025f1bb1b2999258cc4bb03d0
LomadaManasa <55827974+LomadaManasa@users.noreply.github.com>
2024-04-10 13:32:45 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+ backend "s3" {
+    bucket = "statefileusingjenkins"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    encrypt=true
+  }
 }
 provider "aws" {
   region = "ap-northeast-1"
